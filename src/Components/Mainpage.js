62b11237847a9fdb704d7617e5bfbb2f40d9e290
Heisenberk-Karabay <85685449+Heisenberk-Karabay@users.noreply.github.com>
2022-01-28 18:21:16 +0300

    test

---
+++ src/Components/Mainpage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class Mainpage extends Component {
+  render() {
+    return  <div className='container-sm bg-primary'>
+
+                <div class="row">
+                    <div class="col text-center">
+                    1 of 3
+                    </div>
+                    <div class="col text-center">
+                    
+                    </div>
+                    <div class="col text-center">
+                    3 of 3
+                    </div>
+                </div>
+                
+
+            </div>
+
+
+
+            
+    ;
+  }
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Navbar = () =>{
+  return (
+    <div>
+      <h3>Navbar</h3>
+      <div>
+        <img src="./logo.jpg"/>
+      </div>
+    </div>
+  );
+}
+export default Navbar;
