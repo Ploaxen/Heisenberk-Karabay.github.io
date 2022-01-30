91e4add0fe1dc9eaff1284b50981269d69371e26
Heisenberk-Karabay <85685449+Heisenberk-Karabay@users.noreply.github.com>
2022-01-30 20:43:55 +0300

    Footer-Update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Mainpage from './Components/Mainpage';
 import Footer from './Components/Footer';
+import Test from './Components/Test';
 
 function App() {
   return (
     <>
-    
-    <Mainpage />
+  
+    <Footer />
     </>
   );
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,89 +1,98 @@
+import { Button } from 'bootstrap';
 import React, { Component } from 'react';
 
 
 export default class Footer extends Component {
   render() {
-    return <footer className ="page-footer font-small blue pt-4">
+    return <footer className="fixed-bottom bg-dark text-center text-white">
+      
+      <div className="container p-4">
+        
+        <section className="mb-4">
+          
+          <div className='container'>
+            <div className='row justify-content-center'>
+
+              <div className='col-sm-2 col-md-1 mb-1 mb-md-0'>
+                  <button type='button' className='btn btn-secondary'>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-facebook" viewBox="0 0 16 16">
+                          <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                      </svg>
+                  </button>
+
+              </div>
+
+              <div className='col-sm-2 col-md-1 mb-1 mb-md-0'>
+
+                  <button type='button' className='btn btn-secondary'>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16">
+                          <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                      </svg>
+                  </button>
+                
+              </div>
+
+            </div>
+          </div>
+          
+        
+
+        
+          
+       
+        </section>
+      
+        
+    
+        
+        <section className="mb-4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+            repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+            eum harum corrupti dicta, aliquam sequi voluptate quas.
+          </p>
+        </section>
 
     
-    <div className ="container-fluid text-center text-md-left">
-  
-      
-      <div className ="row">
-  
         
-        <div className ="col-md-6 mt-md-0 mt-3">
-  
+    
+        
+        <section className="">
           
-          <h5 className ="text-uppercase">Footer Content</h5>
-          <p>Here you can use rows and columns to organize your footer content.</p>
-  
-        </div>
-        
-  
-        <hr className ="clearfix w-100 d-md-none pb-3" />
-  
-        
-        <div className ="col-md-3 mb-md-0 mb-3">
-  
+          <div className="row justify-content-center gx-5">
+            
+            <div className="col-lg-6 col-md-6 mb-4 mb-md-0">
+            <section className='embed-responsive embed-responsive-1by1'>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1082.7228281142936!2d32.7332058899234!3d39.8652186212458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d3474234dd5eb3%3A0x5a6c30cb932231a0!2sGeomatik%20M%C3%BChendisli%C4%9Fi!5e0!3m2!1sen!2str!4v1643560546193!5m2!1sen!2str" width="400" height="300" allowFullScreen="" loading="lazy"></iframe>
+            </section>
+                    
+              
+            </div>
+            
+    
+            
+            <div className="col-lg-6 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase">tel iletisim bilgieri eposta tel vs</h5>
+    
+              
+            </div>
+            
+    
+            
           
-          <h5 className ="text-uppercase">Links</h5>
-  
-          <ul className ="list-unstyled">
-            <li>
-              <a href="#!">Link 1</a>
-            </li>
-            <li>
-              <a href="#!">Link 2</a>
-            </li>
-            <li>
-              <a href="#!">Link 3</a>
-            </li>
-            <li>
-              <a href="#!">Link 4</a>
-            </li>
-          </ul>
-  
-        </div>
-        
-  
-        
-        <div className ="col-md-3 mb-md-0 mb-3">
-  
+            
+          </div>
           
-          <h5 className ="text-uppercase">Links</h5>
-  
-          <ul className ="list-unstyled">
-            <li>
-              <a href="#!">Link 1</a>
-            </li>
-            <li>
-              <a href="#!">Link 2</a>
-            </li>
-            <li>
-              <a href="#!">Link 3</a>
-            </li>
-            <li>
-              <a href="#!">Link 4</a>
-            </li>
-          </ul>
-  
-        </div>
+        </section>
         
-  
       </div>
       
-  
-    </div>
     
-  
+      
+      
+      
+    </footer>
     
-    <div className ="footer-copyright text-center py-3">© 2020 Copyright:
-      <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-    </div>
-    
-  
-  </footer>;
   }
 }
 
